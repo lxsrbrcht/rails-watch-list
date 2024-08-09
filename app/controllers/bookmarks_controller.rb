2ec416fed06774840ddcd79be4c0ecfde025c674
Alexis Reubrecht <lxsrbrcht@gmail.com>
2024-08-09 17:06:25 +0200

    fully fonctional

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -13,6 +13,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
